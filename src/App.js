0e97100946b51feeb1cb7e1191cca8f34b3e612e
Rafael1302 <almeidaderafael@gmail.com>
2021-08-24 14:32:35 -0300

    adcionando funcionalidades

--- src/App.js
+++ src/App.js
@@ -20,6 +20,62 @@ export default function App (){
     )
   }
 
+  const addNumbScreen=(n)=>{
+    if(n === '+' || n === '-' || n === '*' || n === '/' && operator){
+      console.log("=-*/")
+      setOperator(false)
+      setScreenValue(result + n)
+      return
+    }
+    if(operator){
+      setScreenValue(n)
+      setOperator(false)
+      return
+    }
+    const valueTypedScreen = screenValue + n
+    setScreenValue(valueTypedScreen)
+  }
+
+  const deleteMemory = () =>{
+    setOperator(false)
+    setScreenValue('')
+    setResult(0)
+    setAcumulator(0)
+    return
+  }
+
+  const Operation = (op) =>{
+    if(op == 'bs'){
+      let vscreen = screenValue
+      vscreen = vscreen.substring(0,(vscreen.length-1))
+      setScreenValue(vscreen)
+      setOperator(false)
+      return
+    }
+    try{
+      const r = eval(screenValue)
+      setAcumulator(r)
+      setResult(r)
+      setOperator(true)
+    }catch{
+      setResult('ERROR')
+    }
+  }
+
+  const containerCss = {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignitems: 'center',
+    flexDirection: 'column',
+    width: 300,
+    border: '1px solid #000'
+  }
+
+  const buttonsCss = {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+
   const screenCss = {
     display: 'flex',
     paddingLeft: 20,
@@ -56,7 +112,34 @@ export default function App (){
 
   return(
     <>
+      <div style={containerCss}>
+        <h3>Rafael's React Calculator</h3>
+          {Screen(screenValue, result)}
+        <div style={buttonsCss}>
+            {Btn('AC', deleteMemory)}
+            {Btn('(', ()=>addNumbScreen('('))}
+            {Btn(')', ()=>addNumbScreen(')'))}
+            {Btn('/', ()=>addNumbScreen('/'))}
+            {Btn('7', ()=>addNumbScreen('7'))}
+            {Btn('8', ()=>addNumbScreen('8'))}
+            {Btn('9', ()=>addNumbScreen('9'))}
+            {Btn('*', ()=>addNumbScreen('*'))}
+            {Btn('4', ()=>addNumbScreen('4'))}
+            {Btn('5', ()=>addNumbScreen('5'))}
+            {Btn('6', ()=>addNumbScreen('6'))}
+            {Btn('-', ()=>addNumbScreen('-'))}
+            {Btn('1', ()=>addNumbScreen('1'))}
+            {Btn('2', ()=>addNumbScreen('2'))}
+            {Btn('3', ()=>addNumbScreen('3'))}
+            {Btn('+', ()=>addNumbScreen('+'))}
+            {Btn('0', ()=>addNumbScreen('0'))}
+            {Btn('.', ()=>addNumbScreen('.'))}
+            {Btn('<=', ()=>Operation('bs'))}
+            {Btn('=', ()=>Operation('='))}
 
+
+        </div>
+      </div>
     </>
   );
 }
